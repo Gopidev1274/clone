39229d7365d302a6befd60691991fbb77aba4fcc
Gopidev1274 <gopigamer.1274@gmail.com>
2019-09-19 15:34:03 +0530

    java

---
+++ src/org/clone/java1.java
@@ -0,0 +1,11 @@
+package org.clone;
+
+import java.util.Scanner;
+public class java1 {
+public static void main(String[] args) {
+	Scanner a=new Scanner(System.in);
+	System.out.println("Enter Jdk version");
+	String ver = a.nextLine();
+System.out.println("JDK version is 1.8"+ver);
+}
+}
